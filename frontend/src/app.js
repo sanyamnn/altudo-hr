6156c953a04b24db4364790e98daf3b161aee459
Sanyam Chugh <chughsanyam@gmail.com>
2025-03-30 15:25:50 -0400

    Create app.js

---
+++ frontend/src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Chatbot from "./components/Chatbot";
+
+function App() {
+  return (
+    <div>
+      <h2 style={{ textAlign: "center", padding: 20 }}>Altudo HR Chatbot</h2>
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;
